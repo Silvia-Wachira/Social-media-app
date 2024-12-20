1fb9b0d8f125c9b16c3dc69d5c3ffa4090159a8d
Silvia-Wachira <wachirasilvia8.com>
2024-12-20 22:30:00 +0300

    create routes and axios.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.jsx';
+import ProtectedRoute from './routes/ProtectedRoute.jsx';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-    </Routes>
+      <Route path='/' element={
+        <ProtectedRoute>
+        <Home />
+        </ProtectedRoute>
+      } />
+
+      <Route path="/login/" element={<div>Login</div>} />
+      </Routes>
   );
 }
 
